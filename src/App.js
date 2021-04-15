47f1322f0333ab07d00ff2ce9199f870465548cf
Bondi1 <divya.avanna@gmail.com>
2021-04-16 00:25:03 +1000

    sign in pages and components added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/homepage.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shoppage/shoppage.component'
 import Header from './components/header/header.components'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.components';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
      <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
      </Switch>
     </div>
   );
