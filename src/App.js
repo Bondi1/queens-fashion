fdb64abfe4dc6e535557700214992f5f7a52420d
Bondi1 <divya.avanna@gmail.com>
2021-04-15 12:15:28 +1000

    header component and links to shop and contact pages added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import HomePage from './pages/homepage.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shoppage/shoppage.component'
+import Header from './components/header/header.components'
 
 
 function App() {
   return (
     <div >
+     <Header/>
      <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
